b1a6a9af03f2b439e3801bcc1b59365dae0b9688
Pranav Jain <pranav19sept@gmail.com>
2021-05-28 23:21:13 +0530

    Configured project to pick env vars from .env file Changed database to Postgres Updated .gitignore, requirements.txt, requirements-manual.txt

--- erp/settings.py
+++ erp/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jrcd7rwi)zn%liqwr)a6_$b(5k0s%a^%%h^d^+(xvn0jtgtkz4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jrcd7rwi)zn%liqwr)a6_$b(5k0s%a^%%h^d^+(xvn0jtgtkz4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    # Dotenv Configuration
+    load_dotenv()
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'erp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'postgres'),
     }
 }
 
@@ -105,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.environ.get('DJANGO_TIME_ZONE', 'UTC')
 
 USE_I18N = True
 
